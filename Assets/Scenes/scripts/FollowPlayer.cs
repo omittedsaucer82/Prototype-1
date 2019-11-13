33fbf8c457205289adc74b9692e80f62b9a2d124
omittedsaucer82 <56165659+omittedsaucer82@users.noreply.github.com>
2019-11-13 08:46:42 -0800

    camera following added color tint

--- Assets/Scenes/scripts/FollowPlayer.cs
+++ Assets/Scenes/scripts/FollowPlayer.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject player;
+    // Start is called before the first frame update
     void Start()
     {
         
