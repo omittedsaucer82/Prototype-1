9ca976e472c83a4a39c516e88187657f7b84421e
omittedsaucer82 <56165659+omittedsaucer82@users.noreply.github.com>
2019-11-15 08:16:58 -0800

    finished prototype

--- Assets/Scenes/scripts/PlayerController.cs
+++ Assets/Scenes/scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+    public float VerticalInput;
     void Start()
     {
         
@@ -14,7 +18,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    horizontalInput = Input.GetAxis("Horizontal");
+    forwardInput = Input.GetAxis("Vertical");
      // Move the vehicle forward
-     transform.Translate(Vector3.forward * Time.deltaTime * speed);
+     transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+     transform.Rotate(Vector3.up, horizontalInput);
     }
 }
